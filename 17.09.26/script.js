b6c8e92777d01a9b056aa49d52db8fb686359c6f
oravio-11y <barbalhoj63@gmail.com>
2026-09-17 17:41:43 -0300

    Add files via upload

---
+++ 17.09.26/script.js
@@ -0,0 +1,7 @@
+function mudarTexto() {
+    document.getElementById("Titulo").textContent = "Você clicou no botão!";
+}
+
+function mudarcor() {
+    document.getElementById("mensagem").style.color = "red";
+}
